ed55a25230502a57b49b46c80fcd3c9bac9cfc58
Vi-Linh Nguyen <vilinh27@gmail.com>
2022-09-21 15:29:50 -0500

    add in files for to do app

---
+++ src/Todo/Todo.js
@@ -0,0 +1,11 @@
+//to do item
+
+//data props:
+//title (should not submit wihtout) - required
+//description -optional
+//author -logged in user's name
+//dateCreated -Date.now() - some js lib needed
+//complete(boolean)
+//dateCompleted() - dynamic
+
+//checkbox with complete boolean needed
---
+++ src/Todo/Todolist.js
@@ -0,0 +1,3 @@
+//contains individual to do items
+
+//form needed to add new to do item to to todolist

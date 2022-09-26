5b8174c3669c7ebe3f97b3505faf36a5a42bbe44
Vi-Linh Nguyen <vilinh27@gmail.com>
2022-09-26 13:30:58 -0500

    add in log in

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@
 import './App.css';
 import React from 'react';
 import Todolist from './Todo/Todolist';
+import UserBar from './User/UserBar';
 import CreateTodo from './Todo/CreateTodo';
 import {useState} from 'react';
 
 function App() {
   const initialTodos={
+    title: "first post",
+    content: "content 1",
+    author: "paul"
+  } 
 
-  }
+  const [user,setUser] =useState('')
+  const [posts, setPosts] = useState('')
 
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <div>
-          <CreateTodo/>
+          <UserBar user={user && <CreateTodo user={user}/>} posts={posts} setPosts = {setPosts}/>
           <Todolist/>
         </div>
       </header>

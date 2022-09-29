d52233417b117ac7ee7d9700d6b46950a426d4a9
Vi-Linh Nguyen <vilinh27@gmail.com>
2022-09-29 15:47:34 -0500

    add user reducer in separate file

---
+++ src/reducer.js
@@ -0,0 +1,11 @@
+function userReducer(state, action) {
+  switch (action.type) {
+    case "LOGIN":
+    case "REGISTER":
+      return action.username;
+    case "LOGOUT":
+      return "";
+    default:
+      return state;
+  }
+}

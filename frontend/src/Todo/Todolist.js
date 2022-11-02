c2da8f488e7934ec2afb89a1feae3aa91dcd856b
Vi-Linh Nguyen <vilinh27@gmail.com>
2022-11-01 22:27:48 -0500

    delete button seems to  work kinda
    delete button deletes note on backend but the deleted note still shows up on frontend. The change is only visible on frontend when browser is refreshed

--- frontend/src/Todo/Todolist.js
+++ frontend/src/Todo/Todolist.js
@@ -1,18 +1,56 @@
 //contains individual to do items
 import Todo from './Todo';
-import {useContext} from 'react';
+import {useContext,useEffect,useState} from 'react';
 import {StateContext} from '../contexts';
 import {useResource} from "react-request-hook";
 
 //form needed to add new to do item to to todolist
 
 //fix key at some point: need UUID instead of index or something-------
-export default function Todolist({onRemove,onComplete}){{/*onComplete prop passed in from App.js */}
+export default function Todolist(){{/*onComplete prop passed in from App.js */}
     const {state, dispatch } = useContext(StateContext);
-    const {toDos} = state;
+    const {toDos,user} = state;
 
-    function handleRemove(id){
-        dispatch({type:"DELETE_TODO",id});
+    const [error,setError] = useState(false);
+
+    const [ toDo, removeToDo ] = useResource(({id,title,dateCreated,complete,description, author}) => ({
+        url: `/toDos/${id}`,
+        method: 'delete',
+        data:{id,title,dateCreated,complete,description, author}
+    }))
+
+    useEffect(()=>{
+        if(toDo?.data?.error){
+            setError(true)
+        }
+        //console.log("useEffect for remove note called");
+        if(toDo?.isLoading === false && toDo?.data){
+            console.log("onto the dispatch");
+            dispatch({
+                type:"DELETE_TODO",
+                id:toDo.data.id,
+                title:toDo.data.title,
+                dateCreated:toDo.data.dateCreated,
+                complete:toDo.data.complete,
+                description:toDo.data.description, 
+                author:toDo.data.author
+            });
+        }
+        
+    },[toDo])
+
+    function handleRemove(id,title,dateCreated,complete,description,author){
+        console.log("handleRemove is called");
+        //dispatch({type:"DELETE_TODO",id});
+        removeToDo({
+                id,
+                title,
+                dateCreated,
+                complete,
+                description,
+                author
+        })
+        
     }
 
     function handleComplete(id){
@@ -20,7 +58,7 @@ export default function Todolist({onRemove,onComplete}){{/*onComplete prop passe
     }
     return(
         <div>   
-            {toDos.map((t) =><Todo {...t} key={t.id} onRemove={handleRemove} item={t} onComplete={handleComplete}/>)}{/*pass in onComplete prop for toggle reducer */}
+            {toDos.map((t) =><Todo {...t} key={t.id} onRemove={()=>{handleRemove(t.id,t.title,t.dateCreated,t.complete,t.description,t.author)}} item={t} onComplete={handleComplete}/>)}{/*pass in onComplete prop for toggle reducer */}
             {/*destructure todos array and spread over  todo component with each property */}
         </div>
     )
